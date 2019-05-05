7c812e7721228aa46b2de136b6cb2ef25d3d7bb4
sanjaysureshkumar <43583299+sanjaysureshkumar@users.noreply.github.com>
2019-05-05 16:17:13 +0530

    Update selectionsort.py

--- selectionsort.py
+++ selectionsort.py
@@ -1,7 +1,7 @@
 lst=[]
 n=int(input("Enter the number of elements\n"))
 for i in range(0,n):
-    ele=int(input("Enter the "+str(i)+" th element\n"))
+    ele=int(input("Enter the "+str(i+1)+" th element\n"))
     lst.append(ele)
 for i in range(0,n-1):
     min=lst[i]
